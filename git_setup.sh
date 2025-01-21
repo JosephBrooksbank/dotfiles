29a02a68bcb17d914a861cbef71d5a40bdb6ba68
Joseph Brooksbank <joseph.brooksbank@navexglobal.com>
2025-01-20 18:50:40 -0600

    add git setup file
    I use 'git st' for git status literally 10 times an hour at least

---
+++ git_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git config --global core.editor "nvim";
+git config --global alias.co checkout;
+git config --global alias.br branch;
+git config --global alias.st status;
+git config --global alias.last 'log -1 HEAD';

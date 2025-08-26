c0563b2dad0deb041b8d300ad9b127b43a2a9345
Joseph Brooksbank <joseph.brooksbank@navex.com>
2025-08-26 08:20:27 -0700

    added more git aliases

--- git-aliases.ps1
+++ git-aliases.ps1
@@ -2,4 +2,14 @@ git config --global alias.co checkout;
 git config --global alias.br branch;
 git config --global alias.st status;
 git config --global alias.last 'log -1 HEAD';
+git config set --global push.autoSetupRemote true;
+# create a new branch from latest develop
+git config --global alias.nb '!f() { git fetch origin develop:develop && git checkout -b "$1" develop; }; f'
+# reset current branch to remote
+git config --global alias.rr '!git fetch origin && git reset --hard origin/$(git branch --show-current)'
+
+# https://ma.ttias.be/pretty-git-log-in-one-line/ 
+git config --global alias.logline "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+
+git config --global alias.logs "logline -5"
 
